6a353f58e5b87539c1597cfb6d85deb1d36a245b
churtadov <carohurtado2010@hotmail.com>
2023-11-18 11:50:02 -0600

    Primer commit

---
+++ MVCReactApp/ClientApp/src/App.js
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const App = () => {
+    return (
+        <div className="container-fluid">
+            <div className="row justify-content-sm-center">
+                <div className="col-sm-4">
+                    <div className="card text-center bg-dark mt5">
+                        <div class
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )    
+}
+
+export default App
